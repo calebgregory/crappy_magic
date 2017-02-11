95ac05c4c26b6793e5bd985c7b568c2079c8c1a8
Caleb Gregory <calebgregory@github.com>
2017-02-11 12:58:12 -0600

    Tearing out the csv loading from the bucket; it will receive this data from elsewhere

--- apps/store/lib/store/bucket.ex
+++ apps/store/lib/store/bucket.ex
@@ -1,26 +1,13 @@
 defmodule Store.Bucket do
   @moduledoc """
-  Stores the Items in memory and provides access to these.
+  Stores an Item in memory and provides access to it.
   """
-
-  require CSV
 
   @doc"""
-  Starts a new bucket which holds data populated by the configured csv file.
-
-  Validates csv. If invalid, returns `{:error, :invalid_csv}`
+  Starts a new Agent which holds a new map
   """
   def start_link do
-    csvStream =
-      Application.get_env(:store, :store_csv)
-      |> File.stream!
-
-    if valid?(csvStream) do
-      data = csvStream |> to_map
-      Agent.start_link(fn -> data end)
-    else
-      {:error, :invalid_csv}
-    end
+    Agent.start_link(fn -> %{} end)
   end
 
   @doc """
@@ -30,46 +17,10 @@ defmodule Store.Bucket do
     Agent.get(bucket, &Map.get(&1, slug))
   end
 
-  @doc"""
-  Ordered list of `headers`.
+  @doc """
+  Puts an item at key `slug` in `bucket`
   """
-  defp headers do
-    [
-      :slug,
-      :title,
-      :owner_name,
-      :owner_email,
-      :video_filename,
-      :video_creator_name,
-      :video_creator_web_address,
-      :video_creator_email,
-      :video_creator_instagram_handle,
-      :price,
-      :description,
-      :materials,
-      :manufacture_info,
-      :mature_content
-    ]
-  end
-
-  @doc"""
-  Takes first row of csv (the headers) and compares these to the stored
-  `headers`. If each item at a given index in each of these lists is equal
-  when converted to a string, then the csv is valid.
-  """
-  defp valid?(stream) do
-    CSV.Decoder.decode(stream, separator: ?|)
-    |> Enum.at(0)
-    |> Enum.zip(headers())
-    |> Enum.all?(fn {str, atm} -> str == to_string(atm) end)
-  end
-
-  @doc"""
-  Reduces a csv file stream into a map from string `slug` to `item`.
-  """
-  def to_map(stream) do
-    stream |>
-    CSV.Decoder.decode(separator: ?|, headers: headers())
-    Enum.reduce(%{}, fn(item, acc) -> Map.put(acc, item.slug, item) end)
+  def put(bucket, slug, item) do
+    Agent.update(bucket, &Map.put(&1, slug, item))
   end
 end
--- apps/store/test/store/bucket_test.exs
+++ apps/store/test/store/bucket_test.exs
@@ -1,13 +1,20 @@
-defmodule StoreTest do
+defmodule Store.BucketTest do
   use ExUnit.Case, async: true
-  doctest Store
+  doctest Store.Bucket
 
   setup do
     {:ok, bucket} = Store.Bucket.start_link
     {:ok, bucket: bucket}
   end
 
-  test "gives access to storage", %{bucket: bucket} do
-    assert Store.Bucket.get(bucket, "12345") != nil
+  test "starts fresh", %{bucket: bucket} do
+    assert Store.Bucket.get(bucket, "420") == nil
+  end
+
+  test "stores values", %{bucket: bucket} do
+    assert Store.Bucket.get(bucket, "420") == nil
+
+    :ok = Store.Bucket.put(bucket, "420", %{p: "q", slug: "420"})
+    assert Store.Bucket.get(bucket, "420") == %{p: "q", slug: "420"}
   end
 end
