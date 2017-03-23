35b219e004ec69a315d869cb50c99c4532b6746a
Caleb Gregory <calebgregory@github.com>
2017-03-23 10:18:43 -0500

    A ha - oops ;P

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -42,7 +42,7 @@ export default class App extends Component {
     }
 
     this.setState({ view }, () => {
-      const timeoutId = setTimeout(() => setView('Main'), 1000 * 60 * 10 /* 10 minutes */);
+      const timeoutId = setTimeout(() => this.setView('Main'), 1000 * 60 * 10 /* 10 minutes */);
       this.setState({ timeoutId });
     });
   }
