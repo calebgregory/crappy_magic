973104a99d7f14c15dad16c21029b1eb26f1032b
Caleb Gregory <calebgregory@github.com>
2017-03-26 16:47:18 -0500

    Self-hosting fonts

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,5 +5,6 @@ import App from './containers/App';
 require('./assets/css/normalize.css');
 require('./assets/css/skeleton.css');
 require('./styles.css');
+require('./fonts.css');
 
 render(<App />, document.getElementById('root'));
