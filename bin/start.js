b4f8528f16fdf7244415ac69eedcc511fe969aef
Caleb Gregory <calebgregory@gmail.com>
2017-06-11 16:36:52 -0500

    Adding pwd and shell options for child_process mix

--- bin/start.js
+++ bin/start.js
@@ -4,7 +4,10 @@ const path = require('path');
 const cp = require('child_process');
 
 // start video and item server using mix
-const mix = cp.spawn('mix', ['run', '--no-halt']);
+const mix = cp.spawn('mix', ['run', '--no-halt'], {
+  cwd: path.resolve(__dirname, '..'),
+  shell: true,
+});
 
 mix.stdout.on('data', (data) => {
   console.log(`mix stdout: ${data}`);
@@ -12,6 +15,10 @@ mix.stdout.on('data', (data) => {
 
 mix.stderr.on('data', (data) => {
   console.log(`mix stderr: ${data}`);
+});
+
+mix.on('error', (err) => {
+  console.log(`mix err'd with code ${err}`);
 });
 
 mix.on('close', (code) => {
@@ -27,6 +34,10 @@ node.stdout.on('data', (data) => {
 
 node.stderr.on('data', (data) => {
   console.log(`node stderr: ${data}`);
+});
+
+node.on('error', (err) => {
+  console.log(`node err'd with code ${err}`);
 });
 
 node.on('close', (code) => {
